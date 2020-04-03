b36cf9aca1e466be68d4294f8963e58b6f0048df
Juho F <juho.faarinen@gmail.com>
2020-04-04 02:23:11 +0300

    Vk4 Teht3 mockito

---
+++ Viikko4/Teht3-4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.verkkokauppa;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class KauppaTest {
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    Kauppa k;
+    
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+        k = new Kauppa(varasto, pankki, viite); 
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {        
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+        
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());   
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+    
+    @Test
+    public void ostoksenPaatyttyaTilisiirrollaOikeatArvot() {
+        when(viite.uusi()).thenReturn(42);
+        
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        
+        //ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
+    }
+    
+    @Test
+    public void kaksiEriOstostaPaatyyTilisiirrolleOikeillaArvoilla() {
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(5);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 4));
+        
+        //shoppaillaan
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pätkä", "67890");
+        
+        verify(pankki).tilisiirto(eq("pätkä"), anyInt(), eq("67890"), anyString(), eq(9));
+    }
+    
+    @Test
+    public void kaksiSamaaOstostaPaatyyTilisiirrolleOikeillaArvoillaKunVarastossaTarpeeksi() {
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        
+        //shoppaillaan
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(1);
+        k.tilimaksu("justiina", "12345");
+        
+        verify(pankki).tilisiirto(eq("justiina"), anyInt(), eq("12345"), anyString(), eq(10));
+    }
+    
+    @Test
+    public void vainTuoteJotaVarastossaPaatyyTilisiirrolle() {
+        when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(0);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 4));
+        
+        //shoppaillaan
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pikkarainen", "54321");
+        
+        verify(pankki).tilisiirto(eq("pikkarainen"), anyInt(), eq("54321"), anyString(), eq(5));
+    }
+}
