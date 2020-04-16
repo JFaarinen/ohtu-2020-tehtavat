5713237ad62aeffb10dbbc284636f8b1ca898d1f
Juho F <juho.faarinen@gmail.com>
2020-04-16 22:03:37 +0300

    Vk6 Teht3

---
+++ Viikko6/Teht2-5/QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,69 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // seuraavassa osoitteessa 27.11.2019 päivitetyt tilastot
+        String url = "https://nhl27112019.herokuapp.com/players.txt";
+        // ajan tasalla olevat tilastot osoitteessa
+        // "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+
+        Statistics stats = new Statistics(new PlayerReaderImpl(url));
+          
+        Matcher m = new And( new HasAtLeast(5, "goals"),
+                             new HasAtLeast(5, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+        System.out.println("**********");
+        
+        Matcher m2 = new And(
+                new Not(new HasAtLeast(1, "goals")),
+                new PlaysIn("NYR")
+        );
+        
+        for (Player player : stats.matches(m2)) {
+            System.out.println(player);
+        }
+        
+        System.out.println("**********");
+        
+        Matcher m3 = new And(
+                new HasFewerThan(1, "goals"),
+                new PlaysIn("NYR")
+        );
+                
+        for (Player player : stats.matches(m3)) {
+            System.out.println(player);
+        }
+        
+        System.out.println("**********");
+        
+        Matcher m4 = new Or( new HasAtLeast(20, "goals"),
+                     new HasAtLeast(20, "assists")
+        );
+        
+        for (Player player : stats.matches(m4)) {
+            System.out.println(player);
+        }
+        
+        System.out.println("**********");
+        
+        Matcher m5 = new And(
+                new HasAtLeast(20, "points"),
+                new Or( new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("NJD")
+                )
+        ); 
+        
+        for (Player player : stats.matches(m5)) {
+            System.out.println(player);
+        }
+        
+    }
+}
